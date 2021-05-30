9bafaa7c59d1098176ef86aa5eb042e8d0193e22
Ben Siebert <dev@mctzock.de>
2021-05-30 13:03:40 +0200

    Added BungeeCord Connector

--- src/main/java/net/craftions/api/Api.java
+++ src/main/java/net/craftions/api/Api.java
@@ -4,14 +4,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Api extends JavaPlugin {
 
+    protected static Api instance;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
+        getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static Api getInstance() {
+        return instance;
     }
 }
---
+++ src/main/java/net/craftions/api/bungeecord/BungeeCordConnector.java
@@ -0,0 +1,20 @@
+/**
+ * @copyright (c) 2021 Ben Siebert. All rights resered.
+ * @author Ben Siebert
+ */
+package net.craftions.api.bungeecord;
+
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import net.craftions.api.Api;
+import org.bukkit.entity.Player;
+
+public class BungeeCordConnector {
+
+    public static void connect(Player player, String server){
+        ByteArrayDataOutput out = ByteStreams.newDataOutput();
+        out.writeUTF("Connect");
+        out.writeUTF(server);
+        player.sendPluginMessage(Api.getInstance(), "BungeeCord", out.toByteArray());
+    }
+}
