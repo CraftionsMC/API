e41cb771a6168f816f745aa84477e78581218361
Ben Siebert <dev@mctzock.de>
2021-05-30 13:12:42 +0200

    Added some methods

--- src/main/java/net/craftions/api/game/Game.java
+++ src/main/java/net/craftions/api/game/Game.java
@@ -5,4 +5,48 @@
 package net.craftions.api.game;
 
 public class Game {
+
+    // Global variables
+    /**
+     * The name of the game
+     */
+    private String name;
+    /**
+     * The color code used for the prefix
+     */
+    private String colorCode;
+    /**
+     * The minimum number of players
+     */
+    private Integer minPlayers;
+    /**
+     * The time to start the game
+     */
+    private Integer startTime;
+    /**
+     * The time to end the game
+     */
+    private Integer endTime;
+
+    // Timer variables
+    private Integer _startTimer;
+    private Integer _endTimer;
+
+    // State variables
+    private Boolean isStarting;
+    private Boolean isRunning;
+
+    /**
+     * @return true if the game is currently starting
+     */
+    public Boolean getStarting() {
+        return isStarting;
+    }
+
+    /**
+     * @return true if the game is currently running
+     */
+    public Boolean getRunning() {
+        return isRunning;
+    }
 }
